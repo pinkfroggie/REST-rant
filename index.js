fbb1d764b66551178def59dc55e7ac7a6cca5561
ivy <aibiimaknae@gmail.com>
2022-07-27 12:21:48 -0500

    added read me

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
@@ -5,23 +6,19 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
 
 
-require('dotenv').config()
-const express = require('express')
-const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(process.env.port)
-
+// app.listen(process.env.port)
 
 require('dotenv').config()
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
 app.use('/places', require('./controllers/places'))
 
