2100a9f8d02e1e17c42dd6dda3fb2998e955a76a
ivy <aibiimaknae@gmail.com>
2022-08-08 18:34:54 -0500

    aded places w mock data

--- index.js
+++ index.js
@@ -5,7 +5,27 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+// controllers & routes
 app.use('/places', require('./controllers/places'))
+
+// GET /places
+app.get('/', (req, res) => {
+  let places = [{
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: 'http://placekitten.com/250/250'
+    }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: 'http://placekitten.com/250/250'
+    }]
+    
+  res.render('places/index', { places })
+})
 
 app.get('/', (req, res) => {
   res.render('home')
